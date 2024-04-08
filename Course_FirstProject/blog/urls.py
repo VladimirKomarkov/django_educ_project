288b9f1c66f9706e9bae87094ffaaca89c40e0bb
Neto test <vlkom995@gmail.com>
2024-04-08 14:29:03 +0700

    first.templates(1)

--- Course_FirstProject/blog/urls.py
+++ Course_FirstProject/blog/urls.py
@@ -3,9 +3,4 @@ from blog import views
 
 urlpatterns = [
     path("", views.index),
-    path("index/<int:id>/", views.index),
-    path("access/<int:age>/", views.index),
-    path("about/", views.about),
-    path("contact/", views.contact),
-    path("details/", views.details),
 ]
--- Course_FirstProject/blog/views.py
+++ Course_FirstProject/blog/views.py
@@ -1,47 +1,11 @@
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect, HttpResponseNotFound, \
-    HttpResponseBadRequest, HttpResponseForbidden
+from django.shortcuts import render
 
 
-def index(request, id):
-    people = [None, "Bob", "Sam", "Tom"]
-    if id in range(1, len(people)):
-        return HttpResponse(people[id])
-    else:
-        return HttpResponseNotFound("Not Found")
+def index(request):
+    header = "Данные пользвателя"
+    langs = ["Python", "Java", "C#"]
+    user = {"name": "Tom", "age": 23}
+    address = ("Абрикосовая", 23, 45)
+    data = {"header": header, "langs": langs, "user": user, "address": address}
+    return render(request, "blog/index.html", context=data)
 
-def access(request, age):
-    if age not in range(1, 111):
-        return HttpResponseBadRequest("некорректные данные")
-    if (age > 17):
-        return HttpResponse("Доступ разрешен")
-    else:
-        return HttpResponseForbidden("Доступ заблокирован: недостаточно лет")
-
-def about(request):
-    return HttpResponse('About')
-
-
-def contact(request):
-    return HttpResponseRedirect('/about')
-
-
-def details(request):
-    return HttpResponsePermanentRedirect('/')
-
-
-def user(request):
-    age = request.GET.get('age', 0)
-    name = request.GET.get('name', 'Undefined')
-    return HttpResponse(f"<h2>Имя: {name}, Возраст: {age}</h2>")
-
-
-def products(request, id):
-    return HttpResponse(f"Товар {id}")
-
-
-def comments(request, id):
-    return HttpResponse(f"Комментарий о товаре {id}")
-
-
-def questions(request, id):
-    return HttpResponse(f"Вопросы о товаре {id}")
